42acb83875ca2208035fdbeb07458a20753e0368
froggy1014 <clcl6084@gmail.com>
2022-09-22 23:37:35 +0900

    feat(MainPage.tsx): Insert Image Test Complete

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 
-import { ChakraProps } from '@chakra-ui/react';
+import { Box, ChakraProps, Flex, Image } from '@chakra-ui/react';
+
+import MainHeader from '@components/common/@Layout/MobileLayout/_fragments/MainHeader';
 
 interface MainPageProps extends ChakraProps {}
 
 function MainPage({ ...basisProps }: MainPageProps) {
   return (
     <>
-      <h1>Hello world</h1>
+      <Flex direction="column">
+        <MainHeader px="16px" />
+        <Image
+          objectFit="cover"
+          src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fce9Abm%2FbtrMPm2Klv8%2FRFRk7IVCHQdIEkaxDJgee0%2Fimg.png"
+          alt="page1"
+          boxSize="375px"
+          h="auto"
+        />
+        <Box as="section" w="375px" h="712px" bg="skyblue">
+          Helow
+        </Box>
+        <Box as="section" w="375px" h="712px" bg="light">
+          Helow
+        </Box>
+        <Box as="section" w="375px" h="712px" bg="skyblue">
+          Helow
+        </Box>
+        <Box as="section" w="375px" h="712px" bg="beige">
+          Helow
+        </Box>
+      </Flex>
     </>
   );
 }
