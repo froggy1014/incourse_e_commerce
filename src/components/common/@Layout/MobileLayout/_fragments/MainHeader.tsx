19fa0436b840db2d3cb030ba007f5be148201a97
froggy1014 <clcl6084@gmail.com>
2022-09-22 16:57:25 +0900

    feat(MainHeader.tsx): Drawer feature Added
    1. useDisclosure
    2. Drawer Apply

--- src/components/common/@Layout/MobileLayout/_fragments/MainHeader.tsx
+++ src/components/common/@Layout/MobileLayout/_fragments/MainHeader.tsx
@@ -1,4 +1,17 @@
-import { Flex, IconButton } from '@chakra-ui/react';
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  IconButton,
+  Input,
+  useDisclosure,
+} from '@chakra-ui/react';
 
 import CartIcon from '@components/common/@Icons/System/Cart';
 import MenuIcon from '@components/common/@Icons/System/Menu';
@@ -6,21 +19,43 @@ import MenuIcon from '@components/common/@Icons/System/Menu';
 import Logo from 'generated/icons/Logo';
 
 const MainHeader = () => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
   return (
     <>
       <Flex w="100%" h="80px" justify="space-between" align="center">
         <IconButton
+          onClick={onOpen}
           aria-label="MenuIcon"
           icon={<MenuIcon />}
           bg="transparent"
         />
         <Logo size="sm" />
         <IconButton
+          // onClick={}
           aria-label="CartIcon"
           icon={<CartIcon />}
           bg="transparent"
         />
       </Flex>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent w="313px" maxW="313px">
+          <DrawerCloseButton />
+          <DrawerHeader>카테고리</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder="Type here..." />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 };
