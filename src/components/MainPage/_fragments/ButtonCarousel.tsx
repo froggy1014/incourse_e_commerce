524f7dc1f4b42edbedb289593cb7770995a777aa
froggy1014 <clcl6084@gmail.com>
2022-09-23 16:03:51 +0900

    feat(ButtonCarousel.tsx): Layout and Test Complete

---
+++ src/components/MainPage/_fragments/ButtonCarousel.tsx
@@ -0,0 +1,48 @@
+import Slider from 'react-slick';
+
+import { Box, Button } from '@chakra-ui/react';
+
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+
+function ButtonCarousel() {
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    arrows: false,
+  };
+  return (
+    <>
+      <Slider {...settings}>
+        <Box>
+          <Button variant="review">전체</Button>
+        </Box>
+        <Box>
+          <Button variant="review" w="63px">
+            바스&샴푸
+          </Button>
+        </Box>
+        <Box>
+          <Button variant="review">오일</Button>
+        </Box>
+        <Box>
+          <Button variant="review">로션</Button>
+        </Box>
+        <Box>
+          <Button variant="review">크림</Button>
+        </Box>
+        <Box>
+          <Button variant="review" w="83px">
+            파우더 로션
+          </Button>
+        </Box>
+      </Slider>
+    </>
+  );
+}
+
+export default ButtonCarousel;
