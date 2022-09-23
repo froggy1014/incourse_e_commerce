81088256c7f3792e9707e5a219543c086dd8e443
froggy1014 <clcl6084@gmail.com>
2022-09-23 21:49:51 +0900

    feat(Section3.tsx): Generate Icon and Layout done.

--- src/components/MainPage/_fragments/Section3.tsx
+++ src/components/MainPage/_fragments/Section3.tsx
@@ -5,9 +5,11 @@ import { Box, Button, Container, Text } from '@chakra-ui/react';
 import ButtonCarousel from './ButtonCarousel';
 import SingleCarousel from './SingleCarousel';
 
+import { FloatingActionIcon } from 'generated/icons/FloatingActionIcon';
+
 const Section3 = () => {
   return (
-    <Box w="100%" zIndex="4">
+    <Box w="100%" zIndex="4" position="relative">
       <Text fontSize="26px" align="center" mt="80px">
         인코스런을 <strong>직접 사용해본</strong>
         <br />
@@ -15,6 +17,7 @@ const Section3 = () => {
       </Text>
       <ButtonCarousel />
       <SingleCarousel />
+      <FloatingActionIcon />
     </Box>
   );
 };
