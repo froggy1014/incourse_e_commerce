b030cc8d80511e7e3da1b92c5479bbd90e97c33f
froggy1014 <clcl6084@gmail.com>
2022-09-25 04:40:46 +0900

    feat(QtyIcons.tsx): Qty + - Icon Generate

---
+++ src/generated/icons/QtyMinusIcon.tsx
@@ -0,0 +1,15 @@
+import { Icon, IconProps } from '@chakra-ui/react';
+
+export const QtyMinusIcon = (props: IconProps) => (
+  <Icon
+    w="25px"
+    h="25px"
+    viewBox="0 0 25 25"
+    fill="none"
+    bg="white"
+    roundedLeft="5px"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M17 12H8V13H17V12Z" fill="#4A4D55" />
+  </Icon>
+);
---
+++ src/generated/icons/QtyPlusIcon.tsx
@@ -0,0 +1,16 @@
+import { Icon, IconProps } from '@chakra-ui/react';
+
+export const QtyPlusIcon = (props: IconProps) => (
+  <Icon
+    w="25px"
+    h="25px"
+    viewBox="0 0 25 25"
+    fill="none"
+    bg="white"
+    xmlns="http://www.w3.org/2000/svg"
+    roundedRight="5px"
+  >
+    <path d="M17 12H8V13H17V12Z" fill="#4A4D55" />
+    <path d="M13 17V8H12V17H13Z" fill="#4A4D55" />
+  </Icon>
+);
