971580d6cbbb7ab162eede293246a0b85fb8f720
froggy1014 <clcl6084@gmail.com>
2022-09-26 19:39:41 +0900

    feat(ChatIcon): Create Chat Icon Component

---
+++ src/generated/icons/ChatIcon.tsx
@@ -0,0 +1,18 @@
+import { Icon, IconProps } from '@chakra-ui/react';
+
+export const ChatIcon = (props: IconProps) => (
+  <Icon
+    w="20px"
+    h="20px"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M10.086 2.49902C5.068 2.49902 1 5.89903 1 10.099C1.01139 11.6532 1.56088 13.1554 2.55499 14.35L1.10498 17.667L4.64197 16.174C6.2801 17.1797 8.16781 17.705 10.09 17.69C15.108 17.69 19.175 14.29 19.175 10.09C19.175 5.89003 15.105 2.49902 10.086 2.49902Z"
+      stroke="#FF710B"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </Icon>
+);
