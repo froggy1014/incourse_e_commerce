c0fc2a26d734a60295291af5ecc800208d757a9c
froggy1014 <clcl6084@gmail.com>
2022-09-26 19:44:48 +0900

    feat(DetailSection): 리뷰 간단한 레이아웃 완성

--- src/components/ProductDetailByIdPage/_fragment/DetailSection3.tsx
+++ src/components/ProductDetailByIdPage/_fragment/DetailSection3.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, HStack, Image, Text } from '@chakra-ui/react';
+
+import { ChatIcon } from 'generated/icons/ChatIcon';
+import { RatingIcon } from 'generated/icons/RatingIcon';
 
 const reviews = {
   count: 5,
@@ -76,8 +79,60 @@ const reviews = {
 };
 
 const DetailSection3 = () => {
-  console.log(reviews);
-  return <Box id="ReviewsInfo" w="343px" height="1000px" bg="skyblue"></Box>;
+  return (
+    <>
+      <Flex direction="column" w="100%" my="23px">
+        <Box w="100%" fontSize="12px" pt="10px">
+          <Flex w="100%" justify="space-between">
+            <Text fontWeight="bold">incourse.run</Text>
+            <HStack spacing="1">
+              <RatingIcon />
+              <RatingIcon />
+              <RatingIcon />
+              <RatingIcon />
+              <RatingIcon />
+            </HStack>
+          </Flex>
+          <Text color="gray.600">2021.03.29</Text>
+        </Box>
+        <Text mt="15px" mb="10px">
+          순해서 아이피부에도 자극없이 사용할 수 있어요!
+        </Text>
+        <HStack>
+          <Image
+            src="/images/ReviewImage.png"
+            boxSize="80px"
+            alt="reviewImg"
+            objectFit="cover"
+          />
+          <Image
+            src="/images/ReviewImage.png"
+            boxSize="80px"
+            alt="reviewImg"
+            objectFit="cover"
+          />
+          <Image
+            src="/images/ReviewImage.png"
+            boxSize="80px"
+            alt="reviewImg"
+            objectFit="cover"
+          />
+        </HStack>
+      </Flex>
+      <Box>
+        <HStack>
+          <ChatIcon />
+          <Text fontWeight="bold">인코스런 관리자</Text>
+        </HStack>
+        <Box pl="27px">
+          <Text color="gray.600">2021.03.29</Text>
+          <Text py="20px">
+            감사합니다 고객님! 앞으로 더 노력하는 인코스런이 되겠습니다!
+          </Text>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default DetailSection3;
