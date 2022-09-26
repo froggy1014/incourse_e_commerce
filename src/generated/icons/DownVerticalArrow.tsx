195c2b8b7b51d7cee939f3603606a96985ac7c9c
froggy1014 <clcl6084@gmail.com>
2022-09-26 16:00:11 +0900

    feat(Icon): Generate Vertical Arrow Component

---
+++ src/generated/icons/DownVerticalArrow.tsx
@@ -0,0 +1,19 @@
+import { Icon, IconProps } from '@chakra-ui/react';
+
+export const DownVerticalArrow = (props: IconProps) => (
+  <Icon
+    w="24px"
+    h="24px"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M18.4209 9.328L11.9119 14.9191L5.17188 9.328"
+      stroke="#1A1A1A"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </Icon>
+);
---
+++ src/generated/icons/UpVerticalArrow.tsx
@@ -0,0 +1,19 @@
+import { Icon, IconProps } from '@chakra-ui/react';
+
+export const UpVerticalArrow = (props: IconProps) => (
+  <Icon
+    w="24px"
+    h="24px"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M18.4209 14.672L11.9119 9.08093L5.17188 14.672"
+      stroke="#1A1A1A"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </Icon>
+);
