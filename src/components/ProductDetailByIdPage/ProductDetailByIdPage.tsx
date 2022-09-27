0c4ac12dc10f7dfa1b3230d44eb609469b75fad8
froggy1014 <clcl6084@gmail.com>
2022-09-27 17:49:50 +0900

    feat(ProductDetail): 제품 상세페이지 모달 구현

--- src/components/ProductDetailByIdPage/ProductDetailByIdPage.tsx
+++ src/components/ProductDetailByIdPage/ProductDetailByIdPage.tsx
@@ -9,8 +9,10 @@ import {
   HStack,
   Text,
   VStack,
+  useDisclosure,
 } from '@chakra-ui/react';
 
+import PurchaseModal from '@components/ProductPage/_fragment/PurchaseModal';
 import { SubmitButton } from '@components/common';
 import { RatingIcon } from '@icons/UI';
 
@@ -22,10 +24,80 @@ interface ProductDetailByIdPageProps extends ChakraProps {
   id?: string | string[];
 }
 
+const productInfo = [
+  {
+    id: '0',
+    name: '바스 & 샴푸',
+    description:
+      '머리부터 발끝까지 한번에! 마일드한 세정을 도와주는 올인원 클렌저',
+    price: 27000,
+    capacity: 300,
+    tags: ['올인원', '클렌저', '마일드', '바스 & 샴푸'],
+    avgRate: '4.3',
+    reviewCount: '125',
+    created: '2022-09-26T08:10:13.948Z',
+    imgurl: '/images/ProductProfile.png',
+  },
+  {
+    id: '1',
+    name: '오일',
+    description:
+      '머리부터 발끝까지 한번에! 마일드한 세정을 도와주는 올인원 오일',
+    price: 25000,
+    capacity: 50,
+    tags: ['올인원', '클렌저', '마일드', '오일'],
+    avgRate: '4.3',
+    reviewCount: '125',
+    created: '2022-09-26T08:10:13.948Z',
+    imgurl: '/images/ProductProfile.png',
+  },
+  {
+    id: '2',
+    name: '로션',
+    description:
+      '머리부터 발끝까지 한번에! 마일드한 세정을 도와주는 올인원 로션',
+    price: 20000,
+    capacity: 100,
+    tags: ['올인원', '클렌저', '마일드', '로션'],
+    avgRate: '4.3',
+    reviewCount: '125',
+    created: '2022-09-26T08:10:13.948Z',
+    imgurl: '/images/ProductProfile.png',
+  },
+  {
+    id: '3',
+    name: '크림',
+    description:
+      '머리부터 발끝까지 한번에! 마일드한 세정을 도와주는 올인원 크림',
+    price: 18000,
+    capacity: 310,
+    tags: ['올인원', '클렌저', '마일드', '크림'],
+    avgRate: '4.3',
+    reviewCount: '125',
+    created: '2022-09-26T08:10:13.948Z',
+    imgurl: '/images/ProductProfile.png',
+  },
+  {
+    id: '4',
+    name: '파우더로션',
+    description:
+      '머리부터 발끝까지 한번에! 마일드한 세정을 도와주는 올인원 파우더로션',
+    price: 30000,
+    capacity: 200,
+    tags: ['올인원', '클렌저', '마일드', '파우더로션'],
+    avgRate: '4.3',
+    reviewCount: '125',
+    created: '2022-09-26T08:10:13.948Z',
+    imgurl: '/images/ProductProfile.png',
+  },
+];
+
 function ProductDetailByIdPage({
   id,
   ...basisProps
 }: ProductDetailByIdPageProps) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Box {...basisProps}>
       <Box boxSize="343px" h="300px" bgImage="/images/ProductDetail.png"></Box>
@@ -63,7 +135,7 @@ function ProductDetailByIdPage({
             <Text variant="normal16gray">(리뷰 125개)</Text>
           </HStack>
         </Box>
-        <VStack mb="30px">
+        <VStack mb="30px" onClick={onOpen}>
           <SubmitButton
             title="장바구니"
             variant="btnwhite"
@@ -91,6 +163,11 @@ function ProductDetailByIdPage({
       <DetailSection1 />
       <DetailSection2 />
       <DetailSection3 />
+      <PurchaseModal
+        isOpen={isOpen}
+        onClose={onClose}
+        purchase={['테스트', 2000, `${id}`]}
+      />
     </Box>
   );
 }
