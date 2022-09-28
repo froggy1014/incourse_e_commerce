f9b812156671b5f95907270186ddda4e3cb01e4f
froggy1014 <clcl6084@gmail.com>
2022-09-28 23:45:27 +0900

    feat(Icon): 리뷰작성페이지에 필요한 아이콘 생성

---
+++ src/components/common/@Icons/UI/AddPhotoIcon.tsx
@@ -0,0 +1,33 @@
+import { Icon, IconProps } from '@chakra-ui/react';
+
+export const AddPhotoIcon = (props: IconProps) => (
+  <Icon
+    w="80px"
+    h="80px"
+    viewBox="0 0 80 80"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      x="0.5"
+      y="0.5"
+      width="79"
+      height="79"
+      rx="4.5"
+      stroke="#CBCED6"
+      strokeDasharray="3 3"
+    />
+    <path
+      d="M40 31V49"
+      stroke="#CBCED6"
+      strokeWidth="2"
+      strokeLinecap="round"
+    />
+    <path
+      d="M49 40H31"
+      stroke="#CBCED6"
+      strokeWidth="2"
+      strokeLinecap="round"
+    />
+  </Icon>
+);
