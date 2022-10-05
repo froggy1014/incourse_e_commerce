b07762e5a793cbb1bc7ac6cd98865509fa5753b0
froggy1014 <clcl6084@gmail.com>
2022-10-05 17:53:34 +0900

    feat(제품상세): 리뷰 목록 review card 적용

--- src/components/ProductDetailByIdPage/_fragment/DetailSection3.tsx
+++ src/components/ProductDetailByIdPage/_fragment/DetailSection3.tsx
@@ -15,82 +15,21 @@ import {
 import { ReviewCard } from '@components/common';
 import { EmptyRatingIcon, HalfRatingIcon, RatingIcon } from '@icons/UI';
 
-//   count: 5,
-//   next: 'http://api.example.org/accounts/?page=4',
-//   previous: 'http://api.example.org/accounts/?page=2',
-//   results: [
-//     {
-//       id: 1,
-//       userId: 'kelly',
-//       productId: 1,
-//       rate: 5,
-//       content: 'string',
-//       reviewimageSet: [
-//         {
-//           reviewId: 1,
-//           url: '/images/ReviewImage.png',
-//         },
-//       ],
-//     },
-//     {
-//       id: 2,
-//       userId: 'ovan',
-//       productId: 2,
-//       rate: 4,
-//       content: 'string',
-//       reviewimageSet: [
-//         {
-//           reviewId: 2,
-//           url: '/images/ReviewImage.png',
-//         },
-//       ],
-//     },
-//     {
-//       id: 3,
-//       userId: 'micheal',
-//       productId: 3,
-//       rate: 3,
-//       content: 'string',
-//       reviewimageSet: [
-//         {
-//           reviewId: 3,
-//           url: '/images/ReviewImage.png',
-//         },
-//       ],
-//     },
-//     {
-//       id: 4,
-//       userId: 'john',
-//       productId: 4,
-//       rate: 2,
-//       content: 'string',
-//       reviewimageSet: [
-//         {
-//           reviewId: 4,
-//           url: '/images/ReviewImage.png',
-//         },
-//       ],
-//     },
-//     {
-//       id: 5,
-//       userId: 'evan',
-//       productId: 5,
-//       rate: 1,
-//       content: 'string',
-//       reviewimageSet: [
-//         {
-//           reviewId: 5,
-//           url: '/images/ReviewImage.png',
-//         },
-//       ],
-//     },
-//   ],
-// };
+interface reviewListTypes {
+  content: string;
+  created: string;
+  id: number;
+  nickname: string;
+  rate: number;
+  reviewimageSet: any;
+  userId: number;
+}
 
 const DetailSection3 = () => {
-  const { countNums, total, sum, stars } = useSelector(
+  const { countNums, total, sum, stars, reviewList } = useSelector(
     (state: RootStateOrAny) => state.detailReviews,
   );
+  console.log(reviewList);
   return (
     <>
       <Flex direction="column" w="100%" my="23px">
@@ -174,7 +113,10 @@ const DetailSection3 = () => {
           </HStack>
         </Flex>
       </Flex>
-      <ReviewCard />
+      {reviewList &&
+        reviewList.map((review: reviewListTypes) => {
+          return <ReviewCard key={review.id} review={review} />;
+        })}
     </>
   );
 };
--- src/components/common/ReviewCard/ReviewCard.tsx
+++ src/components/common/ReviewCard/ReviewCard.tsx
@@ -2,48 +2,62 @@ import React from 'react';
 
 import { Box, Flex, HStack, Image, Text } from '@chakra-ui/react';
 
-import { ChatIcon, RatingIcon } from '@icons/UI';
+import { EmptyRatingIcon, RatingIcon } from '@icons/UI';
 
-export const ReviewCard = () => {
+import { formatDate } from '@utils/format';
+
+interface reviewListTypes {
+  review: {
+    content: string;
+    created: string;
+    id: number;
+    nickname: string;
+    rate: number;
+    reviewimageSet: any;
+    userId: number;
+  };
+}
+
+export const ReviewCard = ({ review }: reviewListTypes) => {
+  const { content, created, id, nickname, rate, reviewimageSet, userId } =
+    review;
   return (
     <>
-      <Box w="100%" fontSize="12px" pt="10px">
-        <Flex w="100%" justify="space-between">
-          <Text fontWeight="bold">incourse.run</Text>
-          <HStack spacing="1">
-            <RatingIcon />
-            <RatingIcon />
-            <RatingIcon />
-            <RatingIcon />
-            <RatingIcon />
-          </HStack>
-        </Flex>
-        <Text color="gray.600">2021.03.29</Text>
+      <Box mb="30px">
+        <Box w="100%" fontSize="12px" pt="10px">
+          <Flex w="100%" justify="space-between">
+            <Text fontWeight="bold">{nickname}</Text>
+            <HStack spacing="1">
+              {Array(5)
+                .fill(0)
+                .map((arr, i) => {
+                  if (i < rate) return <RatingIcon key={i} />;
+                  else return <EmptyRatingIcon key={i} />;
+                })}
+            </HStack>
+          </Flex>
+          <Text color="gray.600">{formatDate(created)}</Text>
+        </Box>
+        <Text mt="15px" mb="10px">
+          {content}
+        </Text>
+        <HStack>
+          {reviewimageSet.map(
+            ({ reviewId, url }: { reviewId: number; url: string }) => {
+              return (
+                <Image
+                  key={reviewId}
+                  src={url}
+                  boxSize="80px"
+                  alt="reviewImg"
+                  objectFit="cover"
+                />
+              );
+            },
+          )}
+        </HStack>
       </Box>
-      <Text mt="15px" mb="10px">
-        순해서 아이피부에도 자극없이 사용할 수 있어요!
-      </Text>
-      <HStack>
-        <Image
-          src="/images/ReviewImage.png"
-          boxSize="80px"
-          alt="reviewImg"
-          objectFit="cover"
-        />
-        <Image
-          src="/images/ReviewImage.png"
-          boxSize="80px"
-          alt="reviewImg"
-          objectFit="cover"
-        />
-        <Image
-          src="/images/ReviewImage.png"
-          boxSize="80px"
-          alt="reviewImg"
-          objectFit="cover"
-        />
-      </HStack>
-      <Box>
+      {/* <Box>
         <HStack>
           <ChatIcon />
           <Text fontWeight="bold">인코스런 관리자</Text>
@@ -54,7 +68,7 @@ export const ReviewCard = () => {
             감사합니다 고객님! 앞으로 더 노력하는 인코스런이 되겠습니다!
           </Text>
         </Box>
-      </Box>
+      </Box> */}
     </>
   );
 };
