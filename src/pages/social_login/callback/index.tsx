2bd4ead285d4b7b469659ab2b1d6d909e040f339
froggy1014 <clcl6084@gmail.com>
2022-10-07 22:46:34 +0900

    feat(소셜로그인): 콜백할때 중간 로딩 스피너 추가

--- src/pages/social_login/callback/index.tsx
+++ src/pages/social_login/callback/index.tsx
@@ -2,20 +2,17 @@ import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
+import { Flex, Spinner } from '@chakra-ui/react';
+
+import MobileLayout from '@components/common/@Layout/MobileLayout';
+
 import { socialLoginReq } from '@utils/axios';
 
 function SocialloginCallback({ code, state }: { code: string; state: string }) {
   const router = useRouter();
   const social = { code: code, state: state };
-  // { code: 'access token from messenger' state: 'messenger' }
-
-  // socialLoginReq is custom Axios, it just simply has baseURL,
-  // content-type : 'application/json';
-  socialLoginReq.post('/', social).then((response) => {
-    // response = { isRegister: false, socialToken: ' token from backend '}
+  socialLoginReq.post('/user/social_login/', social).then((response) => {
     document.cookie = 'socialToken=' + response.data.socialToken;
-
-    // re-direct to page depened on isRegister boolean
     if (!response.data.isRegister) router.push('/sign-up');
     else router.push('/');
   });
@@ -24,13 +21,23 @@ function SocialloginCallback({ code, state }: { code: string; state: string }) {
       <Head>
         <title>callback</title>
       </Head>
+      <MobileLayout
+        content={
+          <Flex w="100%" h="100vh" justify="center" align="center">
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="commerse.500"
+              size="xl"
+            />
+          </Flex>
+        }
+      />
     </>
   );
 }
 
-// Get query-string in GetServerSideProps
-// And I wanted to something re-direct feature here not on Code above.
-// But couldn't find way to fetch in Serverside And keep showing me a Error 500.
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const { code, state } = query;
 
