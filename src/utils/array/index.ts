69c39e7bba364d4fcf4a794298c3df52026f8d07
froggy1014 <clcl6084@gmail.com>
2022-10-24 08:34:35 +0900

    chore: orderId로 어레이 정리하는 유틸함수생성

--- src/utils/array/index.ts
+++ src/utils/array/index.ts
@@ -1,1 +1,18 @@
+import { IItem } from './../../components/MypageOrderhistoryPage/OrderHistory.d';
+
 export const removeDuplecate = <T>(arr: T[]): T[] => Array.from(new Set(arr));
+
+export const divideArraybyuuid = (arr: IItem[]) => {
+  const AllorderIds = arr.map((item) => item.orderId);
+  const filteredIds = AllorderIds.filter((c, index) => {
+    return AllorderIds.indexOf(c) === index;
+  });
+
+  const returnArray = Array(filteredIds.length).fill([]);
+
+  filteredIds.forEach((v, i) => {
+    returnArray[i] = arr.filter((item, i) => item.orderId === v);
+  });
+
+  return returnArray;
+};
