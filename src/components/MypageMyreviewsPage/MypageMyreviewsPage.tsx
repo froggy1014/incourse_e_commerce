90892961ea692f1ee70b6476cba7d7d8b93482b4
froggy1014 <clcl6084@gmail.com>
2022-10-27 03:06:41 +0900

    feat(내상품리뷰): useInfiniteQuery Pagination 구현

--- src/components/MypageMyreviewsPage/MypageMyreviewsPage.tsx
+++ src/components/MypageMyreviewsPage/MypageMyreviewsPage.tsx
@@ -1,81 +1,66 @@
 import React, { useState } from 'react';
+import { useInfiniteQuery } from 'react-query';
 
-import {
-  Box,
-  ChakraProps,
-  Divider,
-  Image,
-  Stack,
-  Text,
-} from '@chakra-ui/react';
+import axios from 'axios';
+import { getCookie } from 'cookies-next';
 
-import { MyReviewCard } from '@components/common';
+import { Box, ChakraProps, Divider, Stack, Text } from '@chakra-ui/react';
+
+import { Loading, MyReviewCard } from '@components/common';
+
+import { QUERY_KEY } from '@constants/query-keys';
+
+import { IMyReviews } from './MyReviews';
 
 interface MypageMyreviewsPageProps extends ChakraProps {}
 
-const results = [
-  {
-    id: 1,
-    title: 'Evan',
-    description: '순해서 아이피부에도 자극없이 사용할 수 있어요!',
-    rate: 5,
-    imgUrl: [
-      '/images/ReviewImage.png',
-      '/images/ReviewImage.png',
-      '/images/ReviewImage.png',
-    ],
-    date: '2022-09-11T17:12:52.749378',
-  },
-  {
-    id: 2,
-    title: 'Evan',
-    description: '순해서 아이피부에도 자극없이 사용할 수 있어요!',
-    rate: 4,
-    imgUrl: ['/images/ReviewImage.png'],
-    date: '2022-09-11T17:12:52.749378',
-  },
-  {
-    id: 3,
-    title: 'Evan',
-    description: '순해서 아이피부에도 자극없이 사용할 수 있어요!',
-    rate: 3,
-    imgUrl: [],
-    date: '2022-09-11T17:12:52.749378',
-  },
-  {
-    id: 4,
-    title: 'Evan',
-    description: '순해서 아이피부에도 자극없이 사용할 수 있어요!',
-    rate: 1,
-    imgUrl: ['/images/ReviewImage.png', '/images/ReviewImage.png'],
-    date: '2022-09-11T17:12:52.749378',
-  },
-];
-
 function MypageMyreviewsPage({ ...basisProps }: MypageMyreviewsPageProps) {
+  const user_Id = getCookie('userId');
+  const [page, setPage] = useState(1);
+  const {
+    data: pageInfo,
+    isLoading,
+    fetchNextPage,
+    fetchPreviousPage,
+    hasNextPage,
+    hasPreviousPage,
+    isFetchingNextPage,
+    isFetchingPreviousPage,
+  } = useInfiniteQuery(
+    [QUERY_KEY.MYREVIEWS],
+    async ({ pageParam = page }) => {
+      const res = await axios.get(
+        `https://api.commerce.incourse.run/v1/review/?page=${pageParam}&page_size=4&user_id=${user_Id}`,
+      );
+      return res.data;
+    },
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        const nextPage = page + 1;
+        return nextPage;
+      },
+      getPreviousPageParam: (firstPage, allPages) => {
+        const previousPage = page - 1;
+        return previousPage;
+      },
+    },
+  );
+
+  if (isLoading) return <Loading />;
+
   return (
     <Box {...basisProps}>
       <Text variant="pageTitle">내 상품 리뷰</Text>
       <Text variant="bold16" mb="30px">
         총{' '}
         <Box as="span" color="commerse.500">
-          78
+          {pageInfo?.pages[0].count}
         </Box>
         건
       </Text>
       <Stack divider={<Divider />}>
-        {results.map((result) => {
-          const { id, title, description, rate, imgUrl, date } = result;
-          return (
-            <MyReviewCard
-              key={id}
-              title={title}
-              description={description}
-              rate={rate}
-              imgUrl={imgUrl}
-              date={date}
-            />
-          );
+        {pageInfo?.pages[page - 1].results.map((review: IMyReviews) => {
+          return <MyReviewCard key={review.id} review={review} />;
         })}
       </Stack>
     </Box>
