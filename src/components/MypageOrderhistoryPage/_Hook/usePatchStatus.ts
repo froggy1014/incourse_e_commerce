769ed3c1a45d4d1aca6301ae767557612ea75298
froggy1014 <clcl6084@gmail.com>
2022-11-08 04:13:59 +0900

    feat[주문내역]: Patch Custom Hook 생성

---
+++ src/components/MypageOrderhistoryPage/_Hook/usePatchStatus.ts
@@ -0,0 +1,26 @@
+import { UseMutateFunction, useMutation, useQueryClient } from 'react-query';
+
+import { patchShipStatus } from '@apis/_axios/axiosPatch';
+
+import { QUERY_KEY } from '@constants/query-keys';
+
+type TStatus = {
+  oid: string;
+  status: string;
+};
+
+export function usePatchStatus(): UseMutateFunction<
+  void,
+  unknown,
+  TStatus,
+  unknown
+> {
+  const QueryClient = useQueryClient();
+  const { mutate } = useMutation((data: TStatus) => patchShipStatus(data), {
+    onSuccess: () => {
+      QueryClient.invalidateQueries([QUERY_KEY.MYORDERSSTATUS]);
+    },
+  });
+
+  return mutate;
+}
