5a155eedf1867ac28f9945e926802e6e53f37971
froggy1014 <clcl6084@gmail.com>
2022-11-13 21:43:31 +0900

    refactor[*]: 보일러 플레이트에서 필요없는 파일삭제

--- src/utils/kakao/kakaoInit.tsx
+++
@@ -1,8 +0,0 @@
-export const kakaoInit = () => {
-  const kakao = (window as any).Kakao;
-  if (!kakao.isInitialized()) {
-    kakao.init('130b767dca3e725b223ca2751868488d');
-  }
-
-  return kakao;
-};
