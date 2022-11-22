0bb14ca9d797680d4b20f2c5e380826958302a30
froggy1014 <clcl6084@gmail.com>
2022-11-22 18:00:19 +0900

    feat[메인페이지]: 이벤트 상세보기 클릭
    1. Modal을 뛰움으로써, 이벤트 상세보기 클릭 기능 상쇄

--- src/components/MainPage/_fragments/Section1.tsx
+++ src/components/MainPage/_fragments/Section1.tsx
@@ -1,34 +1,43 @@
+import { useState } from 'react';
+
 import { Container, Image, Text } from '@chakra-ui/react';
 
 import ArrowRightIcon from '@components/common/@Icons/System/ArrowRight';
+import { CompleteModal } from '@components/common/GlobalModal/CompleteModal';
 
 const Section1 = () => {
-  const handleClick = () => {
-    console.log('Section 1 Click!');
-  };
+  const [open, setOpen] = useState(false);
 
   return (
-    <Container centerContent position="relative">
-      <Image
-        objectFit="cover"
-        src="/images/Section1.png"
-        alt="page1"
-        minW="375px"
-        h="auto"
+    <>
+      <Container centerContent position="relative">
+        <Image
+          objectFit="cover"
+          src="/images/Section1.png"
+          alt="page1"
+          minW="375px"
+          h="auto"
+        />
+        <Text
+          w="128px"
+          position="absolute"
+          bottom="200px"
+          left="-14px"
+          variant="normal16"
+          alignItems="center"
+          cursor="pointer"
+          onClick={() => setOpen(!open)}
+        >
+          이벤트 상세보기 <ArrowRightIcon />
+        </Text>
+      </Container>
+      <CompleteModal
+        title="죄송합니다. 이벤트는 종료 되었습니다."
+        isOpen={open}
+        onClose={() => setOpen(!open)}
+        setOpen={setOpen}
       />
-      <Text
-        w="128px"
-        position="absolute"
-        bottom="200px"
-        left="-14px"
-        variant="normal16"
-        alignItems="center"
-        cursor="pointer"
-        onClick={handleClick}
-      >
-        이벤트 상세보기 <ArrowRightIcon />
-      </Text>
-    </Container>
+    </>
   );
 };
 
