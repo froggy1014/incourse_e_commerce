357f5a50ffa3f7a85d1db214567a2863086d8a9b
froggy1014 <clcl6084@gmail.com>
2022-11-22 19:17:48 +0900

    feat[Header]: 로그아웃 기능 추가
    1. 기존 로그아웃 버튼은 있었으나 기능 구현 X
    2. LogoutModal 컴포넌트 가져와 기능 구현

--- src/components/common/@Layout/MobileLayout/_fragments/MainHeader.tsx
+++ src/components/common/@Layout/MobileLayout/_fragments/MainHeader.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
 import {
   Button,
@@ -16,15 +17,19 @@ import {
   useDisclosure,
 } from '@chakra-ui/react';
 
+import LogoutModal from '@components/MypagePage/_fragment/LogoutModal';
 import CartIcon from '@components/common/@Icons/System/Cart';
 import ExitIcon from '@components/common/@Icons/System/Exit';
 import MenuIcon from '@components/common/@Icons/System/Menu';
 import { Logo } from '@icons/UI';
 
+import { ROUTES } from '@constants/routes';
+
 type MainHeaderType = { px?: string };
 
 const MainHeader = ({ px }: MainHeaderType) => {
   const { isOpen, onClose, onOpen } = useDisclosure();
+  const [open, setOpen] = useState(false);
   return (
     <>
       <Flex
@@ -76,26 +81,31 @@ const MainHeader = ({ px }: MainHeaderType) => {
               fontWeight="bold"
             >
               <Text fontSize={'20px'}>카테고리</Text>
-              <Link href="/">
+              <Link href={ROUTES.MAIN}>
                 <a>홈</a>
               </Link>
-              <Link href="/product">
+              <Link href={ROUTES.PRODUCT}>
                 <a>상품보기</a>
               </Link>
-              <Link href="/mypage">
+              <Link href={ROUTES.MYPAGE.MAIN}>
                 <a>마이페이지</a>
               </Link>
             </VStack>
             <Divider mt="4" />
           </DrawerBody>
           <DrawerFooter display="block" px="0">
-            <Button bg="transparent" fontSize="20">
+            <Button
+              bg="transparent"
+              fontSize="20"
+              onClick={() => setOpen(!open)}
+            >
               <ExitIcon />
               <Text ml="5px">로그아웃</Text>
             </Button>
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
+      <LogoutModal isOpen={open} onClose={() => setOpen(!open)} />
     </>
   );
 };
